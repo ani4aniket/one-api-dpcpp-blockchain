e40f979fafa3fc28380a9cdcd6b7bf22a7d14f01
u62316 <u62316@s001-n012.aidevcloud>
2021-02-22 07:40:48 -0800

    final commit

--- run-chain.sh
+++ run-chain.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
-/bin/echo "##" $(whoami) is compiling BlockChain DPCPP
+/bin/echo "##" $(whoami) is compiling DPCPP_Essentials to crack md5 encryption and reveal insecure passwords
 dpcpp src/*.cpp -o bin/TestChain -std=c++11
 if [ $? -eq 0 ]; then bin/TestChain; fi
+
